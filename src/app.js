5bb842a8df6cec12ae90f8df638c55e5b8277ed2
Diego <willslipoknotpsico@gmail.com>
2023-10-16 14:54:52 -0500

    aaaaa

--- src/app.js
+++ src/app.js
@@ -1,30 +1,26 @@
 import express from "express";
 import morgan from "morgan";
-import cookieParser from 'cookie-parser'
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import userRoutes from './routes/user.routes.js';
-import votoRoutes from './routes/voto.routes.js'
-import candidatosRoutes from './routes/candidatos.routes.js'
-import resultadosRoutes from './routes/resultados.routes.js'
+import votoRoutes from './routes/voto.routes.js';
+import candidatosRoutes from './routes/candidatos.routes.js';
+import resultadosRoutes from './routes/resultados.routes.js';
 
 const app = express();
 app.use(express.json());
 
 const allowedOrigins = [
     'https://jal-pte-aranda2023-front1.vercel.app',
-    'https://containers-us-west-88.railway.app:5586', 
-  ];
-  
-  app.use(cors({
-    origin: function(origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    'https://containers-us-west-88.railway.app',
+];
+
+app.use(cors({
+    origin: allowedOrigins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
-  }));
+    optionsSuccessStatus: 204,
+}));
 
 app.use(morgan('dev'));
 app.use(cookieParser());
